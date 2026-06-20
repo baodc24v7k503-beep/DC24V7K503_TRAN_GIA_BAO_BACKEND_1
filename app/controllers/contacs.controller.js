a194623aa8c5f7ffa160afec63b375136923f93b
Gia Bao <baodc24v7k503@vlvh.ctu.edu.vn>
2026-06-20 13:32:05 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contacs.controller.js
@@ -0,0 +1,21 @@
+exports.create = (req, res) => {
+  res.json({ message: 'Create handler' });
+};
+exports.findAll = (req, res) => {
+  res.json({ message: 'Find all handler' });
+};
+exports.findOne = (req, res) => {
+  res.json({ message: 'Find one handler' });
+};
+exports.update = (req, res) => {
+  res.json({ message: 'Update handler' });
+};
+exports.delete = (req, res) => {
+  res.json({ message: 'Delete handler' });
+};
+exports.deleteAll = (req, res) => {
+  res.json({ message: 'Delete all handler' });
+};
+exports.findAllFavorite = (req, res) => {
+  res.json({ message: 'Find all favorite handler' });
+};
