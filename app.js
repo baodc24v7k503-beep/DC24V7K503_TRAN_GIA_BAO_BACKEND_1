7d994728dde46a9c75fbc222e67021dd2386bb20
Gia Bao <baodc24v7k503@vlvh.ctu.edu.vn>
2026-06-20 13:40:20 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const ApiError = require('./app/api-error');
+
 const app = express();
 
 const contactRoutes = require('./app/routes/contact.route');
@@ -12,6 +14,18 @@ app.use('/api/contacts', contactRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Contact Book application.' });
+});
+
+app.use("/api/contacts", contactRoutes);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
